d5e33c9efe4a28904fb2b8c9e43c306ae05d071e
@s.roertgen <sroertgen@gmail.com>
2025-01-04 16:31:00 +0100

    Add basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,14 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex flex-row">
+	<div class="basis-1/4">
+		<nav class="flex flex-col">
+			<a href="/">EduVocs</a>
+			<a href="/search">Suche</a>
+			<a href="/contact">Kontakt</a>
+		</nav>
+	</div>
+
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
 <h1 class="text-2xl">Vokabulardatenbank</h1>
 
-<a href="/search">Suche</a>
