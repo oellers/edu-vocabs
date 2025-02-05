085a7680e3b112e6350b17049046696958a07844
oellers <81564160+oellers@users.noreply.github.com>
2025-02-05 19:09:14 +0100

    (result) refined menu

--- src/lib/components/ResultMenu.svelte
+++ src/lib/components/ResultMenu.svelte
@@ -1,16 +1,18 @@
 <script>
 	let { result, hideButtons = [] } = $props();
+	import { t } from 'svelte-i18n';
 	import { db } from '$lib/db';
 	import { VOCAB_PROPERTIES as vp } from '$lib/constants';
 	import InternalLinkIcon from '$lib/icons/InternalLinkIcon.svelte';
 	import ExternalLinkIcon from '$lib/icons/ExternalLinkIcon.svelte';
-	import VocabDownload from './VocabDownload.svelte';
-	import { t } from 'svelte-i18n';
+	import VocabDownload from '$lib/components/VocabDownload.svelte';
+	import SourceCodeIcon from '$lib/icons/SourceCodeIcon.svelte';
+
 	const vocabDistribution = $derived(result[vp.distribution]?.map((e) => $db.index.store[e]));
 </script>
 
 <div class="join join-vertical">
-	{#if !hideButtons.includes('details')}
+	{#if !hideButtons.includes(vp.id)}
 		<a
 			class="btn btn-outline join-item btn-sm h-auto border-slate-400 py-2"
 			aria-label={$t('voc.preview')}
@@ -18,7 +20,7 @@
 			><InternalLinkIcon /> {$t('details')}
 		</a>
 	{/if}
-	{#if result[vp.describedAt] && !hideButtons.includes('external')}
+	{#if result[vp.describedAt] && !hideButtons.includes(vp.describedAt)}
 		<a
 			class="btn btn-outline join-item btn-sm h-auto border-slate-400 py-2"
 			aria-label={$t('buttons.external')}
@@ -29,7 +31,18 @@
 			{$t('buttons.external')}
 		</a>
 	{/if}
-	{#if vocabDistribution && vocabDistribution.length && !hideButtons.includes('download')}
+	{#if result[vp.repo] && !hideButtons.includes(vp.repo)}
+		<a
+			class="btn btn-outline join-item btn-sm h-auto border-slate-400 py-2"
+			aria-label={$t('buttons.repo')}
+			href={result[vp.repo]}
+			target="_blank"
+		>
+			<SourceCodeIcon />
+			{$t('buttons.repo')}
+		</a>
+	{/if}
+	{#if vocabDistribution && vocabDistribution.length && !hideButtons.includes(vp.distribution)}
 		<VocabDownload {vocabDistribution} />
 	{/if}
 </div>
--- src/routes/voc/[id]/+page.svelte
+++ src/routes/voc/[id]/+page.svelte
@@ -2,20 +2,32 @@
 	import Result from '$lib/components/Result.svelte';
 	import Vocab from '$lib/components/Vocab.svelte';
 	import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
+	import { VOCAB_PROPERTIES as vp } from '$lib/constants';
 	import { t } from 'svelte-i18n';
 	import { db } from '$lib/db';
 	let { data } = $props();
+	const result = $db.index?.store?.[data.id];
 </script>
 
 <svelte:head>
-	<title>{$t('brand')}: {$t('voc.view')}</title>
+	<title>{$t('brand')}: {$t('voc.detailview')}</title>
 </svelte:head>
 
 {#if $db.initialized}
 	<div class="flex w-full flex-col">
-		{#if $db.index?.store?.[data.id]}
-			<h1 class="text-2xl">{$t('voc.view')}</h1>
-			<Result id={data.id} hideButtons={['details']} />
+		<div class="text-md breadcrumbs">
+			<ul>
+				<li>
+					<a aria-label={$t('menu.search')} href="/search">{$t('menu.search')}</a>
+				</li>
+				<li>
+					<span>{$t('voc.label')}: {result[vp.name]}</span>
+				</li>
+			</ul>
+		</div>
+		{#if result}
+			<h1 class="text-2xl">{$t('voc.detailview')}</h1>
+			<Result id={data.id} hideButtons={[vp.id]} />
 			<Vocab id={data.id} />
 		{:else}
 			<p>{$t('voc.notfound')}</p>
