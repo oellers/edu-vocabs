7d276f7de7cb48b07d60560eeda55f13e16c5eba
oellers <81564160+oellers@users.noreply.github.com>
2025-02-05 18:38:57 +0100

    (fix) single view vocabulary

--- src/routes/voc/[id]/+page.svelte
+++ src/routes/voc/[id]/+page.svelte
@@ -1,17 +1,26 @@
 <script>
 	import Result from '$lib/components/Result.svelte';
 	import Vocab from '$lib/components/Vocab.svelte';
-	let { data } = $props();
+	import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
 	import { t } from 'svelte-i18n';
+	import { db } from '$lib/db';
+	let { data } = $props();
 </script>
 
 <svelte:head>
 	<title>{$t('brand')}: {$t('voc.view')}</title>
 </svelte:head>
 
-<h1 class="text-2xl">{$t('voc.view')}</h1>
-
-<div class="flex w-full flex-col">
-	<Result id={data.id} hideButtons={['details']} />
-	<Vocab id={data.id} />
-</div>
+{#if $db.initialized}
+	<div class="flex w-full flex-col">
+		{#if $db.index?.store?.[data.id]}
+			<h1 class="text-2xl">{$t('voc.view')}</h1>
+			<Result id={data.id} hideButtons={['details']} />
+			<Vocab id={data.id} />
+		{:else}
+			<p>{$t('voc.notfound')}</p>
+		{/if}
+	</div>
+{:else}
+	<LoadingSpinner />
+{/if}
