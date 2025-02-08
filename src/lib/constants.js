814ab857f4b70f0bb4cf0ee4dcfc9692e3d72439
oellers <81564160+oellers@users.noreply.github.com>
2025-02-09 00:00:40 +0100

    (model) update

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,20 +1,27 @@
 export const VOCAB_PROPERTIES = {
-	id: 'id',
+	id: 'id', // http://purl.org/dc/terms/identifier
 	type: 'type', // http://www.w3.org/1999/02/22-rdf-syntax-ns#type
-	name: 'name', // https://schema.org/name
-	description: 'description', // https://schema.org/description
+	name: 'title', // http://purl.org/dc/terms/title
+	description: 'description', // http://purl.org/dc/terms/description
 	about: 'about', // https://schema.org/about
+	creator: 'creator', // http://purl.org/dc/terms/creator
 	issued: 'issued', // http://purl.org/dc/terms/issued
+	modified: 'modified', // http://purl.org/dc/terms/modified
+	audience: 'audience', // https://schema.org/audience
+	language: 'language', // http://purl.org/dc/terms/language
+	license: 'license', // http://purl.org/dc/terms/license
 	educationalLevel: 'educationalLevel', // https://schema.org/educationalLevel
+	endpointUrl: 'endpointUrl', // https://www.w3.org/ns/dcat#endpointURL
 	describedAt: 'P973', // http://www.wikidata.org/prop/direct/P973
 	maintainedBy: 'P126', // http://www.wikidata.org/prop/direct/P126
 	usedBy: 'P1535', // http://www.wikidata.org/prop/direct/P1535
 	repo: 'P1324', // http://www.wikidata.org/prop/direct/P1324
 	compliesWith: 'P5009', // http://www.wikidata.org/prop/direct/P5009
-	format: 'P2701', // http://www.wikidata.org/prop/direct/P2701
+	format: 'fomrat', // http://purl.org/dc/terms/format
 	fileFormat: 'fileFormat', // https://schema.org/fileFormat
 	distribution: 'distribution', // https://schema.org/distribution
-	rawVocab: 'rawVocab',
+	rawVocab: 'rawVocab', // https://eduvocs/
+	keywords: 'keywords', // https://schema.org/keywords
 	contentUrl: 'contentUrl' // https://schema.org/contentUrl
 };
 
