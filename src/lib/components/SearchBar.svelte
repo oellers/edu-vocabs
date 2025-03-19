78e843528106950cbb6cc0e0bbd2602c906590e6
@s.roertgen <sroertgen@gmail.com>
2025-03-19 08:16:54 +0100

    Move SearchBar to dedicated component

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { db, search } from '$lib/db';
+	import { t } from 'svelte-i18n';
+	import SearchIcon from '$lib/icons/SearchIcon.svelte';
+</script>
+
+<label class="input input-bordered flex items-center max-lg:w-3/4 lg:w-full">
+	<input
+		class="grow"
+		type="text"
+		name="searchbox"
+		onkeyup={(e) => search(e)}
+		bind:value={$db.query}
+		placeholder="{$t('search')}..."
+	/>
+	<SearchIcon />
+</label>
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-	import { db, search } from '$lib/db';
+	import { search } from '$lib/db';
 	import DropdownFilter from '$lib/components/DropdownFilter.svelte';
 	import Results from '$lib/components/Results.svelte';
-	import SearchIcon from '$lib/icons/SearchIcon.svelte';
 	import ResetFilters from '$lib/components/ResetFilters.svelte';
+	import SearchBar from '$lib/components/SearchBar.svelte';
 	import { VOCAB_PROPERTIES as vp } from '$lib/constants';
 	import { t } from 'svelte-i18n';
 	import DropdownSort from '$lib/components/DropdownSort.svelte';
@@ -17,17 +17,7 @@
 	<form onsubmit={(e) => search(e)}>
 		<div>
 			<div class="flex w-full gap-2 md:flex-row">
-				<label class="input input-bordered flex items-center max-lg:w-3/4 lg:w-full">
-					<input
-						class="grow"
-						type="text"
-						name="searchbox"
-						onkeyup={(e) => search(e)}
-						bind:value={$db.query}
-						placeholder="{$t('search')}..."
-					/>
-					<SearchIcon />
-				</label>
+				<SearchBar />
 				<div class="ml-auto flex items-center md:justify-end lg:w-1/4">
 					<ResetFilters />
 				</div>
